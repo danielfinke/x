ccf24793292caf88908963de37e1dfb37beb0bab
Daniel Finke <danielfinke2011@gmail.com>
2021-01-09 21:38:37 -0800

    Small sync up with 725800f

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'styles/globals.scss';
 
-import { AppProps } from 'next/app';
-import { ReactElement } from 'react';
+import type { AppProps } from 'next/app';
+import type { ReactElement } from 'react';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { ReactElement } from 'react';
+import type { ReactElement } from 'react';
 
 export default function Home(): ReactElement {
-  return <h1>Hello world!</h1>;
+  return <h1>Hello, world!</h1>;
 }
