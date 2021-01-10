7b52217c72121e104bd19e40d7c4880bbf86ee1c
Daniel Finke <danielfinke2011@gmail.com>
2021-01-09 22:49:57 -0800

    Added <html> lang attr and <Head> page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,17 @@ import type { ReactElement } from 'react';
 
 import 'styles/globals.scss';
 
+import Head from 'next/head';
+import { description, name } from 'package.json';
+
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>{name}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
