104a82cb3b518dc6ca4eab905d77b7472ada7cd6
Daniel Finke <danielfinke2011@gmail.com>
2021-01-09 21:20:30 -0800

    Add ESLint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import 'styles/globals.css';
 
 import { AppProps } from 'next/app';
+import { ReactElement } from 'react';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
-};
+}
