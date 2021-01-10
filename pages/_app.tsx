25dd402e6f9b4bc4458335384f49266639df4297
Daniel Finke <danielfinke2011@gmail.com>
2021-01-09 20:57:04 -0800

    Declare MyApp param type (AppProps)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import 'styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
+import { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 };
