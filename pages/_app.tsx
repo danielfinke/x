9a9fec3c7780148ff2dd3e3b4d0c9798f5492e30
Daniel Finke <danielfinke2011@gmail.com>
2021-01-09 21:33:35 -0800

    Add Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,6 @@ import 'styles/globals.scss';
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 
-export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+export default function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
 }
