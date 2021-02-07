5c05105d91ce89cc02d0dc4c83cd320495dcb634
Daniel Finke <danielfinke2011@gmail.com>
2021-02-06 18:08:29 -0800

    Create testing library render wrapper function to always provide default theme

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -3,12 +3,21 @@ import Index from 'pages/index';
 import { ThemeProvider } from 'styled-components';
 import defaultTheme from 'themes/default.json';
 
-test('renders index page', () => {
-  const { getByText } = render(
-    <ThemeProvider theme={defaultTheme}>
-      <Index />
-    </ThemeProvider>
+type ExtraRenderParams = Parameters<typeof render>;
+/**
+ * Wrap testing library `render` to always provide the default styled components theme
+ *
+ * @param param0 arguments normally provided to `render`
+ * @returns normal `render` return value
+ */
+const renderWithTheme = (...[children, ...args]: ExtraRenderParams) =>
+  render(
+    <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>,
+    ...args
   );
+
+test('renders index page', () => {
+  const { getByText } = renderWithTheme(<Index />);
   const helloWorldElement = getByText('Hello, world!');
 
   expect(helloWorldElement).toBeInTheDocument();
