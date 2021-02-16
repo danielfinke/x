cd322490fa8b7a4d97991ad11629c2e8b7c0a950
Daniel Finke <danielfinke2011@gmail.com>
2021-02-15 19:20:51 -0800

    refactor: app Metadata

---
+++ components/pages/Metadata.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react';
+import type { MetadataProps } from 'types/components/pages/Metadata';
+
+import Head from 'next/head';
+import {
+  description as packageDescription,
+  name as packageName
+} from 'package.json';
+
+const Metadata: FC<MetadataProps> = ({
+  description = packageDescription,
+  title = packageName
+}: MetadataProps) => (
+  <Head>
+    <meta name="description" content={description} />
+    <title>{title}</title>
+  </Head>
+);
+
+export default Metadata;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps } from 'next/app';
 import type { ReactElement } from 'react';
 
+import Metadata from 'components/pages/Metadata';
 import StyledApp from 'components/pages/StyledApp';
-import Head from 'next/head';
-import { description, name } from 'package.json';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
     <>
-      <Head>
-        <title>{name}</title>
-        <meta name="description" content={description} />
-      </Head>
+      <Metadata />
       <StyledApp>
         <Component {...pageProps} />
       </StyledApp>
