e18d67619379ace2b0609019ddf1c4a731471cea
Daniel Finke <danielfinke2011@gmail.com>
2021-02-15 20:07:04 -0800

    Add processDirectory (& jest-next-dynamic for dynamic imports in jest)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from 'react';
 
-import HelloWorld from 'components/apps/HelloWorld';
+import processDirectory from 'utils/processDirectory';
 
 export default function Home(): ReactElement {
-  return <HelloWorld />;
+  return <processDirectory.HelloWorld.Component />;
 }
