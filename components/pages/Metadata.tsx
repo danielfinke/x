82846a52c145f2bd855be7588a2d185aef939ef9
Daniel Finke <danielfinke2011@gmail.com>
2021-05-20 08:50:55 -0700

    Remove props type from Metadata

--- components/pages/Metadata.tsx
+++ components/pages/Metadata.tsx
@@ -10,7 +10,7 @@ import {
 const Metadata: FC<MetadataProps> = ({
   description = packageDescription,
   title = packageName
-}: MetadataProps) => (
+}) => (
   <Head>
     <meta name="description" content={description} />
     <title>{title}</title>
