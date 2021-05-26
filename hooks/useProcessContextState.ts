c0eda06e799b12e766cf2ade1b9e677f9532d72c
Daniel Finke <danielfinke2011@gmail.com>
2021-05-26 16:01:39 -0700

    Remove getStartupProcesses

--- hooks/useProcessContextState.ts
+++ hooks/useProcessContextState.ts
@@ -1,10 +1,10 @@
 import type { ProcessContextState } from 'types/contexts/process';
 
 import { useState } from 'react';
-import getStartupProcesses from 'utils/processDirectory';
+import { HelloWorld, Taskbar } from 'utils/processDirectory';
 
 const useProcessContextState = (): ProcessContextState => {
-  const [processes] = useState(getStartupProcesses());
+  const [processes] = useState({ HelloWorld, Taskbar });
 
   return { processes };
 };
--- utils/processDirectory.ts
+++ utils/processDirectory.ts
@@ -1,29 +1,12 @@
-import type { Processes } from 'types/contexts/process';
+import type { Process } from 'types/contexts/process';
 
-import Taskbar from 'components/system/Taskbar';
 import dynamic from 'next/dynamic';
 
-const STARTUP_PROCESSES = ['HelloWorld', 'Taskbar'];
-
-const processDirectory: Processes = {
-  HelloWorld: {
-    Component: dynamic(() => import('components/apps/HelloWorld')),
-    hasWindow: true
-  },
-  Taskbar: {
-    Component: Taskbar
-  }
+export const HelloWorld: Process = {
+  Component: dynamic(() => import('components/apps/HelloWorld')),
+  hasWindow: true
 };
 
-const getStartupProcesses = (): Processes =>
-  STARTUP_PROCESSES.reduce((acc: Processes, id) => {
-    const process = processDirectory[id];
-
-    if (process) {
-      acc[id] = processDirectory[id];
-    }
-
-    return acc;
-  }, {});
-
-export default getStartupProcesses;
+export const Taskbar: Process = {
+  Component: dynamic(() => import('components/system/Taskbar'))
+};
