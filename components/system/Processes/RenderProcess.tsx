cfbf55031a8432378d9846ba8eb98ee79f77b988
Daniel Finke <danielfinke2011@gmail.com>
2021-05-27 15:38:06 -0700

    Create withWindow HOC for RenderProcess

--- components/system/Processes/RenderProcess.tsx
+++ components/system/Processes/RenderProcess.tsx
@@ -1,17 +1,17 @@
-import type { FC } from 'react';
+import type { ComponentType, FC } from 'react';
 import type { Process } from 'types/contexts/process';
 
 import dynamic from 'next/dynamic';
 
 const Window = dynamic(() => import('components/system/Window'));
 
-const RenderProcess: FC<Process> = ({ Component, hasWindow }) =>
-  hasWindow ? (
-    <Window>
-      <Component />
-    </Window>
-  ) : (
+const withWindow = (Component: ComponentType) => (
+  <Window>
     <Component />
-  );
+  </Window>
+);
+
+const RenderProcess: FC<Process> = ({ Component, hasWindow }) =>
+  hasWindow ? withWindow(Component) : <Component />;
 
 export default RenderProcess;
