99e282f7902eae70e3d69fce314ca74644189954
Daniel Finke <danielfinke2011@gmail.com>
2021-05-26 17:46:04 -0700

    Extract renderProcess fn from ProcessLoader

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,15 +1,16 @@
 import type { FC } from 'react';
+import type { Process } from 'types/contexts/process';
 
 import RenderProcess from 'components/system/Processes/RenderProcess';
 import { ProcessConsumer } from 'contexts/process';
 
+const renderProcess = ([id, process]: [string, Process]) => (
+  <RenderProcess key={id} {...process} />
+);
+
 const ProcessLoader: FC = () => (
   <ProcessConsumer>
-    {({ processes }) =>
-      Object.entries(processes).map(([id, process]) => (
-        <RenderProcess key={id} {...process} />
-      ))
-    }
+    {({ processes }) => Object.entries(processes).map(renderProcess)}
   </ProcessConsumer>
 );
 
