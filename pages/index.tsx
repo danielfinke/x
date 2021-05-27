b5755da897b6491850d0d8be02ab3e0213e941ef
Daniel Finke <danielfinke2011@gmail.com>
2021-05-27 16:04:51 -0700

    Lift Taskbar to index.tsx (outside of ProcessLoader)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { FC } from 'react';
 
 import Desktop from 'components/system/Desktop';
 import ProcessLoader from 'components/system/Processes/ProcessLoader';
+import Taskbar from 'components/system/Taskbar';
 import { ProcessProvider } from 'contexts/process';
 
 const Home: FC = () => (
   <Desktop>
     <ProcessProvider>
-      <ProcessLoader />
+      <Taskbar />
+      {/* <ProcessLoader /> */}
     </ProcessProvider>
   </Desktop>
 );
--- utils/processDirectory.ts
+++ utils/processDirectory.ts
@@ -2,11 +2,8 @@ import type { Process } from 'types/contexts/process';
 
 import dynamic from 'next/dynamic';
 
+// eslint-disable-next-line import/prefer-default-export
 export const HelloWorld: Process = {
   Component: dynamic(() => import('components/apps/HelloWorld')),
   hasWindow: true
-};
-
-export const Taskbar: Process = {
-  Component: dynamic(() => import('components/system/Taskbar'))
 };
