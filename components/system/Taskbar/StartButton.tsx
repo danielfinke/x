eb5b8b6b08a8937d07d9e46a54a8f5c5ab87003c
Daniel Finke <danielfinke2011@gmail.com>
2021-05-26 17:59:57 -0700

    Add StartButton tooltip

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -3,7 +3,9 @@ import type { FC } from 'react';
 import StyledStartButton from 'styles/components/system/Taskbar/StyledStartButton';
 
 const StartButton: FC = () => (
-  <StyledStartButton type="button">x</StyledStartButton>
+  <StyledStartButton title="Start" type="button">
+    x
+  </StyledStartButton>
 );
 
 export default StartButton;
