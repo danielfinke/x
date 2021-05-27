10ddc99b8f731f34bf24d36647f020ae88d23639
Daniel Finke <danielfinke2011@gmail.com>
2021-05-27 15:35:58 -0700

    Refactor ProcessLoader process rendering yet again

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,17 +1,15 @@
 import type { FC } from 'react';
-import type { Processes } from 'types/contexts/process';
 
 import RenderProcess from 'components/system/Processes/RenderProcess';
 import { ProcessConsumer } from 'contexts/process';
 
-const renderProcesses = (processes: Processes) =>
-  Object.entries(processes).map(([id, process]) => (
-    <RenderProcess key={id} {...process} />
-  ));
-
 const ProcessLoader: FC = () => (
   <ProcessConsumer>
-    {({ processes }) => renderProcesses(processes)}
+    {({ processes }) =>
+      Object.entries(processes).map(([id, process]) => (
+        <RenderProcess key={id} {...process} />
+      ))
+    }
   </ProcessConsumer>
 );
 
