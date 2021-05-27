5e5af1198ecf50f697967990461121f60a6708a1
Daniel Finke <danielfinke2011@gmail.com>
2021-05-27 12:48:13 -0700

    Add system font stack to <body>

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     overscroll-behavior: none;
   }
 
