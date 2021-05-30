baa1aa8d8a16317f2804de7ac10c4b377c9cd26f
Daniel Finke <danielfinke2011@gmail.com>
2021-05-30 14:06:59 -0700

    Create useDoubleClick hook for activating FileEntry els

--- components/system/Files/FileEntry.tsx
+++ components/system/Files/FileEntry.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 
 import { useProcesses } from 'contexts/process';
+import useDoubleClick from 'hooks/useDoubleClick';
 import useFileInfo from 'hooks/useFileInfo';
 import { useCallback } from 'react';
 import Button from 'styles/common/Button';
@@ -14,11 +15,11 @@ type FileEntryProps = {
 const FileEntry: FC<FileEntryProps> = ({ name, path }) => {
   const { icon, pid } = useFileInfo(path);
   const { open } = useProcesses();
-  const onActivate = useCallback(() => open(pid), [open, pid]);
+  const onClick = useCallback(() => open(pid), [open, pid]);
 
   return (
     <StyledFileEntry>
-      <Button title={name} onClick={onActivate} onKeyDown={onActivate}>
+      <Button title={name} onClick={useDoubleClick(onClick)}>
         <figure>
           <img src={icon} alt={name} />
           <figcaption>{name}</figcaption>
---
+++ hooks/useDoubleClick.ts
@@ -0,0 +1,30 @@
+import type { MouseEventHandler } from 'react';
+
+import { useCallback, useRef } from 'react';
+
+type DoubleClick = (
+  handler: MouseEventHandler,
+  timeout?: number
+) => MouseEventHandler;
+
+const useDoubleClick: DoubleClick = (handler, timeout = 500) => {
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const onClick = useCallback<MouseEventHandler>(
+    (event) => {
+      if (!timer.current) {
+        timer.current = setTimeout(() => {
+          timer.current = null;
+        }, timeout);
+      } else {
+        clearTimeout(timer.current);
+        handler(event);
+        timer.current = null;
+      }
+    },
+    [handler, timeout]
+  );
+
+  return onClick;
+};
+
+export default useDoubleClick;
