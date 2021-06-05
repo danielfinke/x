b5da5d9c75651a3d0efbea31710e00528fb19979
Daniel Finke <danielfinke2011@gmail.com>
2021-06-05 15:50:58 -0700

    Style StyledWindow (box-shadow, height, outline)

--- styles/components/system/Window/StyledWindow.ts
+++ styles/components/system/Window/StyledWindow.ts
@@ -6,7 +6,10 @@ type StyledWindowProps = {
 
 const StyledWindow = styled.section<StyledWindowProps>`
   background-color: ${({ theme }) => theme.colors.window};
+  box-shadow: 0 0 20px rgba(0, 0, 0, 50%);
   display: ${({ minimized = false }) => (minimized ? 'none' : 'block')};
+  height: 100%;
+  outline: 1px solid rgba(0, 0, 0, 25%);
 `;
 
 export default StyledWindow;
