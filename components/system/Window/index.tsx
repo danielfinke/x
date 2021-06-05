3436d8adb4746d61e8db3ae9f587622354fa384f
Daniel Finke <danielfinke2011@gmail.com>
2021-06-05 10:54:58 -0700

    Enable window resizing using react-rnd

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -3,20 +3,31 @@ import type { FC } from 'react';
 
 import Titlebar from 'components/system/Window/Titlebar';
 import { useProcesses } from 'contexts/process';
+import useResizable from 'hooks/useResizable';
+import { Rnd } from 'react-rnd';
 import StyledWindow from 'styles/components/system/Window/StyledWindow';
+import rndDefaults from 'utils/rndDefaults';
 
 const Window: FC<ProcessComponentProps> = ({ children, id }) => {
   const {
     processes: {
-      [id]: { minimized }
+      [id]: { maximized, minimized }
     }
   } = useProcesses();
+  const { height, width, updateSize } = useResizable(maximized);
 
   return (
-    <StyledWindow minimized={minimized}>
-      <Titlebar id={id} />
-      {children}
-    </StyledWindow>
+    <Rnd
+      enableResizing={!maximized}
+      size={{ height, width }}
+      onResizeStop={updateSize}
+      {...rndDefaults}
+    >
+      <StyledWindow minimized={minimized}>
+        <Titlebar id={id} />
+        {children}
+      </StyledWindow>
+    </Rnd>
   );
 };
 
--- styles/components/system/Window/StyledWindow.ts
+++ styles/components/system/Window/StyledWindow.ts
@@ -7,7 +7,6 @@ type StyledWindowProps = {
 const StyledWindow = styled.section<StyledWindowProps>`
   background-color: ${({ theme }) => theme.colors.window};
   display: ${({ minimized = false }) => (minimized ? 'none' : 'block')};
-  position: absolute;
 `;
 
 export default StyledWindow;
