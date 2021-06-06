a8880e88973f75d9be526c17d9b12503c836ddc7
Daniel Finke <danielfinke2011@gmail.com>
2021-06-05 17:43:10 -0700

    Improve FileEntry styling

--- styles/defaultTheme/colors.ts
+++ styles/defaultTheme/colors.ts
@@ -2,6 +2,21 @@ const OPAQUE_WHITE = 'rgba(255, 255, 255, 80%)';
 
 const colors = {
   background: '#000',
+  fileEntry: {
+    background: `hsl(206deg 77% 70% / 25%)`,
+    border: 'hsl(206deg 77% 100% / 30%)',
+    text: '#fff',
+    textShadow: `
+      0 0 1px rgba(0, 0, 0, 75%),
+      0 0 2px rgba(0, 0, 0, 50%),
+      0 0 3px rgba(0, 0, 0, 25%),
+      0 1px 1px rgba(0, 0, 0, 75%),
+      0 1px 2px rgba(0, 0, 0, 50%),
+      0 1px 3px rgba(0, 0, 0, 25%),
+      0 2px 1px rgba(0, 0, 0, 75%),
+      0 2px 2px rgba(0, 0, 0, 50%),
+      0 2px 3px rgba(0, 0, 0, 25%)`
+  },
   highlight: '#76b9ed',
   startButton: '#fff',
   taskbar: {
--- styles/styled.ts
+++ styles/styled.ts
@@ -6,6 +6,12 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       background: string;
+      fileEntry: {
+        background: string;
+        border: string;
+        text: string;
+        textShadow: string;
+      };
       highlight: string;
       startButton: string;
       taskbar: {
@@ -33,6 +39,11 @@ declare module 'styled-components' {
       clock: {
         fontSize: string;
         width: string;
+      };
+      fileEntry: {
+        fontSize: string;
+        iconSize: string;
+        letterSpacing: string;
       };
       fileManager: {
         columnGap: string;
