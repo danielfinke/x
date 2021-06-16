b693e5b1295695b4043b3fbefb982b72b5ee541d
Daniel Finke <danielfinke2011@gmail.com>
2021-06-15 21:34:00 -0700

    Save session to file (broken)

--- contexts/session/useSessionContextState.ts
+++ contexts/session/useSessionContextState.ts
@@ -1,9 +1,9 @@
 import type { Size } from 'components/system/Window/RndWindow/useResizable';
 import type { Dispatch, SetStateAction } from 'react';
 import type { Position } from 'react-rnd';
-import type themes from 'styles/themes';
 
-import { useState } from 'react';
+import { useFileSystem } from 'contexts/fileSystem';
+import { useEffect, useState } from 'react';
 
 type WindowState = {
   position?: Position;
@@ -14,16 +14,50 @@ type WindowStates = {
   [id: string]: WindowState;
 };
 
-export type SessionContextState = {
-  setThemeName: Dispatch<SetStateAction<string>>;
-  setWindowStates: Dispatch<SetStateAction<WindowStates>>;
-  themeName: keyof typeof themes;
+type Session = {
+  themeName: string;
   windowStates: WindowStates;
 };
 
+export type SessionContextState = {
+  setThemeName: Dispatch<SetStateAction<string>>;
+  setWindowStates: Dispatch<SetStateAction<WindowStates>>;
+} & Session;
+
+const SESSION_FILE = '/session.json';
+
 const useSessionContextState = (): SessionContextState => {
+  const { fs } = useFileSystem();
+  const [sessionLoaded, setSessionLoaded] = useState(false);
   const [themeName, setThemeName] = useState('');
   const [windowStates, setWindowStates] = useState<WindowStates>({});
+
+  useEffect(() => {
+    if (sessionLoaded) {
+      fs?.writeFile(
+        SESSION_FILE,
+        JSON.stringify({
+          themeName,
+          windowStates
+        })
+      );
+    }
+  }, [fs, sessionLoaded, themeName, windowStates]);
+
+  useEffect(
+    () =>
+      fs?.readFile(SESSION_FILE, (error, contents) => {
+        if (contents) {
+          const session: Session = JSON.parse(contents.toString());
+
+          setThemeName(session.themeName);
+          setWindowStates(session.windowStates);
+        }
+
+        setSessionLoaded(true);
+      }),
+    [fs]
+  );
 
   return { setThemeName, setWindowStates, themeName, windowStates };
 };
