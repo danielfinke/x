6e65365e33e76bed9af8add019a16c5f5faa5e90
Daniel Finke <danielfinke2011@gmail.com>
2021-06-18 18:37:10 -0700

    Refactor auto window resizing into a hook and remove border offset

--- components/apps/V86/useV86ScreenSize.ts
+++ components/apps/V86/useV86ScreenSize.ts
@@ -1,7 +1,7 @@
 import type { EventCallback, V86Starter } from 'components/apps/V86/types';
 import type { CSSProperties } from 'react';
 
-import { useSession } from 'contexts/session';
+import useWindowSize from 'components/system/Window/useWindowSize';
 import { useCallback, useEffect } from 'react';
 import { useTheme } from 'styled-components';
 import { stripUnit } from 'utils/functions';
@@ -9,46 +9,29 @@ import { stripUnit } from 'utils/functions';
 const SET_SCREEN_GFX = 'screen-set-size-graphical';
 const SET_SCREEN_TXT = 'screen-set-size-text';
 
-const BORDER_OFFSET = 3;
-
 const useV86ScreenSize = (
   id: string,
   emulator: V86Starter | null
 ): CSSProperties => {
-  const { setWindowStates } = useSession();
   const {
     sizes: {
-      titleBar,
       window: { lineHeight }
     }
   } = useTheme();
-
-  const updateWindowSize = useCallback(
-    (height: number, width: number) =>
-      setWindowStates((currentWindowStates) => ({
-        ...currentWindowStates,
-        [id]: {
-          size: { height, width }
-        }
-      })),
-    [id, setWindowStates]
-  );
+  const { updateWindowSize } = useWindowSize(id);
 
   const setScreenGfx = useCallback<EventCallback>(
-    ([width, height]) =>
-      updateWindowSize(height + stripUnit(titleBar.height), width),
-    [titleBar.height, updateWindowSize]
+    ([width, height]) => updateWindowSize(height, width),
+    [updateWindowSize]
   );
 
   const setScreenText = useCallback<EventCallback>(
     ([cols, rows]) =>
       updateWindowSize(
-        rows * stripUnit(lineHeight) +
-          stripUnit(titleBar.height) +
-          BORDER_OFFSET,
+        rows * stripUnit(lineHeight),
         (cols / 2 + 4) * stripUnit(lineHeight)
       ),
-    [lineHeight, titleBar.height, updateWindowSize]
+    [lineHeight, updateWindowSize]
   );
 
   useEffect(() => {
@@ -63,9 +46,7 @@ const useV86ScreenSize = (
 
   return {
     font: `${lineHeight} monospace`,
-    lineHeight,
-    position: 'relative',
-    top: BORDER_OFFSET - 1
+    lineHeight
   };
 };
 
---
+++ components/system/Window/useWindowSize.ts
@@ -0,0 +1,35 @@
+import { useSession } from 'contexts/session';
+import { useCallback } from 'react';
+import { useTheme } from 'styled-components';
+import { stripUnit } from 'utils/functions';
+
+type WindowSize = {
+  updateWindowSize: (height: number, width: number) => void;
+};
+
+const useWindowSize = (id: string): WindowSize => {
+  const { setWindowStates } = useSession();
+  const {
+    sizes: { titleBar }
+  } = useTheme();
+
+  const updateWindowSize = useCallback(
+    (height: number, width: number) =>
+      setWindowStates((currentWindowStates) => ({
+        ...currentWindowStates,
+        [id]: {
+          size: {
+            height: height + stripUnit(titleBar.height),
+            width
+          }
+        }
+      })),
+    [id, setWindowStates, titleBar.height]
+  );
+
+  return {
+    updateWindowSize
+  };
+};
+
+export default useWindowSize;
