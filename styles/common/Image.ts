fdfeded4269c1cabd782caf53d6db0208a693728
Daniel Finke <danielfinke2011@gmail.com>
2021-06-19 10:40:06 -0700

    Don't show common image element until it's loaded

--- styles/common/Image.ts
+++ styles/common/Image.ts
@@ -5,12 +5,17 @@ import { cleanUpBufferUrl } from 'utils/functions';
 
 const onLoad: ReactEventHandler<HTMLImageElement> = ({ target }) => {
   const img = target as HTMLImageElement;
+
+  img.style.setProperty('visibility', 'visible');
+
   if (img.src.startsWith('blob:')) cleanUpBufferUrl(img.src);
 };
 
 const Image = styled.img.attrs({
   draggable: false,
   onLoad
-})``;
+})`
+  visibility: hidden;
+`;
 
 export default Image;
