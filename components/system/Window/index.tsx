df006c2b84a59bee1ee8de6b271c5c1ec00d735c
Daniel Finke <danielfinke2011@gmail.com>
2021-06-19 16:39:18 -0700

    Adjust outline/shadow for background windows

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -5,6 +5,7 @@ import StyledWindow from 'components/system/Window/StyledWindow';
 import Titlebar from 'components/system/Window/Titlebar';
 import useFocusable from 'components/system/Window/useFocusable';
 import { useProcesses } from 'contexts/process';
+import { useSession } from 'contexts/session';
 import { useRef } from 'react';
 
 import RndWindow from './RndWindow';
@@ -12,15 +13,18 @@ import RndWindow from './RndWindow';
 const Window: FC<ProcessComponentProps> = ({ children, id }) => {
   const {
     processes: {
-      [id]: { backgroundColor, minimized }
+      [id]: { backgroundColor, minimized = false }
     }
   } = useProcesses();
+  const { foregroundId } = useSession();
+  const isForeground = id === foregroundId;
   const windowRef = useRef<HTMLElement>(null);
   const { zIndex, ...focusableProps } = useFocusable(id, windowRef);
 
   return (
     <RndWindow id={id} style={{ zIndex }}>
       <StyledWindow
+        foreground={isForeground}
         minimized={minimized}
         ref={windowRef}
         style={{ backgroundColor }}
